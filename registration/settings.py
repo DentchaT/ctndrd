e75b6c501bf24c5e79fb189f0ad637d365202c6f
Atwiine James <atwiinej.lnm@gmail.com>
2025-12-24 16:40:18 -0800

    cc

--- registration/settings.py
+++ registration/settings.py
@@ -92,8 +92,11 @@ ASGI_APPLICATION = 'registration.asgi.application'
 
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'channels.layers.InMemoryChannelLayer'
-    }
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
 }
 #-------------------------------------------------------------------------
 #---------------------code by Dr.James Atwiine----------------------------
